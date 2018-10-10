cb6fa9c1a0bb3cdc79081ed6599e8bb4e5033f03
Pablo David Aguilar Barco <pablo.aguilar@mobilityscm.com>
2018-10-09 22:20:59 -0600

    Se agrega delete y put

--- Lab05/dist/routes/SkuRouter.js
+++ Lab05/dist/routes/SkuRouter.js
@@ -39,18 +39,14 @@ class SkuRouter {
         }
     }
     /**
-     * POST one hero
+     * POST sku
      */
     postSku(req, res) {
         let sku1 = req.body;
-        console.log("sku1");
-        console.log(sku1);
-        console.log("body");
-        console.log(req.body);
         if (sku1) {
             skus.push(sku1);
             res.statusCode = 201;
-            res.status(200).send({
+            res.status(201).send({
                 message: "Success",
                 status: res.status
             });
@@ -64,12 +60,63 @@ class SkuRouter {
         }
     }
     /**
+     * PUT one sku
+     */
+    putSku(req, res) {
+        let sku1 = req.body;
+        let index = skus.findIndex(sku => sku.sku === req.params.id);
+        if (index < 0) {
+            res.status(404).send({
+                message: "No sku found with the given id.",
+                status: res.status
+            });
+        }
+        else if (sku1) {
+            skus[index] = sku1;
+            res.statusCode = 204;
+            res.status(204).send({
+                message: "Success",
+                status: res.status
+            });
+        }
+        else {
+            res.statusCode = 400;
+            res.status(400).send({
+                message: "Bad request could cast to sku.",
+                status: res.status
+            });
+        }
+    }
+    /**
+     * DELETE one sku
+     */
+    deleteSku(req, res) {
+        let index = skus.findIndex(sku => sku.sku === req.params.id);
+        if (index > 0) {
+            delete skus[index];
+            res.statusCode = 204;
+            res.status(204).send({
+                message: "Success",
+                status: res.status
+            });
+        }
+        else {
+            res.statusCode = 404;
+            res.status(404).send({
+                message: "No sku found with the given id.",
+                status: res.status
+            });
+        }
+    }
+    /**
      * Take each handler, and attach to one of the Express.Router's
      * endpoints.
      */
     init() {
         this.router.get("/", this.getAll);
         this.router.get("/:id", this.getOne);
+        this.router.put("/:id", this.putSku);
+        this.router.delete("/:id", this.deleteSku);
         this.router.post("/", this.postSku);
     }
 }
