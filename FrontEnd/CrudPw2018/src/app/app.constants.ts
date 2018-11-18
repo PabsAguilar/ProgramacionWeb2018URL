604b6f96d2d2a7e66ed66aaf40adf15e029047d0
Pablo Aguilar <pabs.aguilar2806@gmail.com>
2018-11-18 11:37:55 -0600

    Se mueve proyecto

--- FrontEnd/CrudPw2018/src/app/app.constants.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable()
-export class Configuration {
-    public server = 'http://localhost:3000/';
-    public apiUrl = 'api/v1/';
-    public serverWithApiUrl = this.server + this.apiUrl;
-}
--- FrontEnd/CrudPw2018/src/app/models/sku.ts
+++
@@ -1,9 +0,0 @@
-export interface Sku {
-  _id: string;
-  sku: string;
-  description: string;
-  createDate: Date;
-  image: string;
-  family: string;
-  hide: boolean;
-}
